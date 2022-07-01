5b9530952bdc3b5369aff36a4ccf34bc4ae058f0
gustavoesposito <espositogustavo796@gmail.com>
2022-07-01 11:44:17 -0300

    att

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,8 @@
 import Home from './pages/Home';
 import Contato from './pages/Contato';
 import Empresa from './pages/Empresa';
-import Navbar from './components/Navbar';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path='/empresa' element={<Empresa/>}></Route>
             <Route path='/contato' element={<Contato/>}></Route>
           </Routes>
+          <Footer/>
         </Router>
 
       </header>
--- my-app/src/components/layout/Footer.js
+++ my-app/src/components/layout/Footer.js
@@ -1,6 +1,23 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer(){
 return(
-    
+ <footer>
+        <p>Nossas Redes Sociais</p>
+
+    <ul className={styles.social_list}>
+        <li>
+            <FaFacebook/>
+            </li>
+        <li>
+            <FaInstagram/>
+        </li>
+        <li>
+            <FaLinkedin/>
+            </li>
+    </ul>
+    </footer>
 )
 }
 
